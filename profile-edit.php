7f5914926d7850738645cdad58ffad78eddfb858
Mark AI <chanthavong_12@hotmail.com>
2024-03-27 09:20:57 +0700

    improve: profile form

--- profile-edit.php
+++ profile-edit.php
@@ -5,6 +5,53 @@ if (!($user)) {
     header('Location: login.php');
     exit;
 }
+
+if(isset($_POST['submit'])) {
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 0;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+    
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+    
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+        } else {
+        echo "Sorry, there was an error uploading your file.";
+        }
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,7 +62,7 @@ if (!($user)) {
 </head>
 <body>
     <h1>Update Profile</h1>
-    <form action="" method="post">
+    <form action="" method="post" enctype="multipart/form-data">
         <p>
             <label for="name">Name</label>
             <input type="text" name="name" id="name" value="<?php echo $user['name']; ?>">
@@ -26,7 +73,7 @@ if (!($user)) {
         </p>
         <p>
             <label for="image">Image</label>
-            <input type="file" accept="image/*" name="image" id="image">
+            <input type="file" accept="image/*" name="fileToUpload" id="image">
         </p>
         <p>
             <input type="submit" name="submit" value="Update">
